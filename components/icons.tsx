d9ac2f07421516c7f8f0384509c5c4202fb93aa9
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-21 10:16:22 -0500

    updates

---
+++ components/icons.tsx
@@ -0,0 +1,148 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {
+// 	faGithub,
+// 	faLinkedin,
+// 	faDev,
+// 	faMedium,
+// 	faTwitter,
+// 	faReact,
+// 	faJava,
+// 	faPython,
+// 	faAngular,
+// 	faGit,
+// 	faNode,
+// 	faWordpress,
+// 	faStripe,
+// 	faGoogle,
+// 	faAdobe,
+// 	faFigma,
+// 	faYarn,
+// 	faNpm,
+// 	faUnsplash
+// } from '@fortawesome/free-brands-svg-icons';
+
+// import { HerokuIcon } from './svg-icons';
+import {
+	faHerokuIcon,
+	faVercelIcon,
+	faGraphQLIcon,
+	faNextIcon,
+	faTypeScriptIcon,
+	faAuth0Icon
+} from '../lib/fas-custom-integration';
+
+type CardIconProps = {
+	twitter: string;
+	linkedin: string;
+	react: string;
+	heroku: string;
+	vercel: string;
+	props: string | number;
+};
+
+const Icons = ({
+	linkedin,
+	react,
+	heroku,
+	vercel
+}: CardIconProps) => {
+	console.log(typeof faHerokuIcon);
+	console.log(typeof faVercelIcon);
+	return (
+		<div className='align-middle justify-right text-right content-right inline-block'>
+			<ul className='align-middle'>
+				<li className='align-middle'>
+					{' '}
+					{vercel === '' ? (
+						<></>
+					) : (
+						<a
+							aria-label='react'
+							target='__blank'
+							href={vercel}
+							className='bg-black text-white hover:text-white hover:bg-iconHover transition-colors leading-normal duration-500 text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-1 rounded-full focus:outline-none'
+						>
+							{vercel === '' ? (
+								<></>
+							) : (
+								<FontAwesomeIcon
+									icon={faVercelIcon}
+									className='flex align-middle text-center font-extrabold text-1xl pb-1'
+									size='lg'
+								/>
+							)}
+						</a>
+					)}{' '}
+					<a
+						aria-label='linkedin'
+						target='__blank'
+						href={linkedin}
+						className='bg-black text-white hover:text-white hover:bg-iconHover transition-colors leading-relaxed duration-500 text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-1 rounded-full focus:outline-none'
+					>
+						<FontAwesomeIcon
+							icon={faNextIcon}
+							className='align-middle text-center font-extrabold text-1xl pb-1'
+							size='lg'
+						/>
+					</a>{' '}
+					<a
+						aria-label='linkedin'
+						target='__blank'
+						href={linkedin}
+						className='bg-black text-white hover:text-white hover:bg-iconHover transition-colors leading-relaxed duration-500 text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-1 rounded-full focus:outline-none'
+					>
+						<FontAwesomeIcon
+							icon={faGraphQLIcon}
+							className='align-middle text-center font-extrabold text-1xl pb-1'
+							size='lg'
+						/>
+					</a>{' '}
+					{heroku === '' ? (
+						<></>
+					) : (
+						<a
+							aria-label='heroku'
+							target='__blank'
+							href={heroku}
+							className='bg-black text-white hover:text-white hover:bg-iconHover transition-colors leading-normal duration-500 text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-1 rounded-full focus:outline-none'
+						>
+							{react === '' ? (
+								<></>
+							) : (
+								<FontAwesomeIcon
+									icon={faTypeScriptIcon}
+									className='flex align-middle text-center font-extrabold text-1xl'
+									size={'sm'}
+								/>
+							)}
+						</a>
+					)}{' '}
+					{heroku === '' ? (
+						<></>
+					) : (
+						<a
+							aria-label='auth0'
+							target='__blank'
+							href={heroku}
+							className='bg-black text-white hover:text-white hover:bg-iconHover transition-colors leading-normal duration-500 text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-1 rounded-full focus:outline-none'
+						>
+							{react === '' ? (
+								<></>
+							) : (
+								<FontAwesomeIcon
+									icon={faAuth0Icon}
+									className='flex align-middle text-center font-extrabold text-1xl'
+									size={'sm'}
+								/>
+							)}
+						</a>
+					)}{' '}
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Icons;
+
+// CUSTOM ICONS https://medium.com/@nsisodiya/hacking-font-awesome-for-custom-svg-icons-ea762118fa7b
