848cf5938f471c3605b9c8b8f6fa3b8090ae0cef
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-21 09:54:00 -0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # svg-library
 svg library
+
+- https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/pages/index.js
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+
+const Index = () => {
+	return (
+		<div>
+			<a>This is an SVG Library</a>
+		</div>
+	);
+};
+
+export default Index;
